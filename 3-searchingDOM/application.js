2b65722eaf30afd61da6d970630835ecdc8e969b
Fabio Quinzi <fabio.quinzi@gmail.com>
2015-02-28 17:16:54 +0100

    added multipleselect and searchingDOM example

---
+++ 3-searchingDOM/application.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//selecting all the list items direct child of #tours
+	$("#tours > li")
+
+	//selecting all the tours from Asia and all the tours that are OnSale
+	$(".asia, .sale")
+
+	//selecting only the first url
+	$("#torus li:first")
+	
+	//selecting only even rows to add striping colour style
+	$("#tours > li:even")
+});
